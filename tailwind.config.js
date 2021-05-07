27abe60cc619fd7e9bffdc7750a1847979c3d29b
Tayo Akosile <akosilety9@gmail.com>
2021-05-07 09:45:43 -0600

    Updated:Added BackgroundImages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      primary: ['Lexend Deca', 'sans-serif'],
-      secondary: ['Inter', 'sans-serif'],
+    extend: {
+      screen: {
+        xs: '420',
+      },
+      fontFamily: {
+        primary: ['Lexend Deca', 'sans-serif'],
+        secondary: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        bodyColor: '#090b1a',
+        card: '#1b1938',
+        span: '#aa5cdb',
+        overlay: '#1c1938',
+      },
+      backgroundImage: theme => ({
+        'mobile': "url('/images/image-header-mobile.jpg')",
+        'desktop': "url('/images/image-header-desktop.jpg')",
+       }),
+      zIndex: {
+        '-50': '-50',
+      },
     },
-    colors: {
-      body: '#090b1a',
-      card: '#1b1938',
-      span: '#aa5cdb',
-    },
-    extend: {},
   },
   variants: {
     extend: {},
