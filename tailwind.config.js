0ce85da12d168d5960752ba5573c61423f6b9673
Tayo Akosile <akosilety9@gmail.com>
2021-05-07 07:47:20 -0600

    Create :Started new roject

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      primary: ['Lexend Deca', 'sans-serif'],
+      secondary: ['Inter', 'sans-serif'],
+    },
+    colors: {
+      body: '#090b1a',
+      card: '#1b1938',
+      span: '#aa5cdb',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
